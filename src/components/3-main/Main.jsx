f554a045f5c076c23a0471143e552d617ebc13fc
RebOussama <o_rebaii@estin.dz>
2024-12-17 12:03:37 +0100

    projects commit

--- src/components/3-main/Main.jsx
+++ src/components/3-main/Main.jsx
@@ -3,12 +3,13 @@ import  './Main.css'
 
 
 const myProjects=[
-  {index:1,cat:'html&css',imgPath:'./1.jpg',title:'Project Name 1 :',subtitle:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus ad praesentium nam! Totam sapiente vitae porro dolores, maiores, autem pariatur fuga doloribus.',link:'#',githubLink:'#',moreLink:'#'},
-  {index:2,cat:'react',imgPath:'./1.jpg',title:'Project Name 2 :',subtitle:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus ad praesentium nam! Totam sapiente vitae porro dolores, maiores, autem pariatur fuga doloribus.',link:'#',githubLink:'#',moreLink:'#'},
-  {index:3,cat:'js',imgPath:'./1.jpg',title:'Project Name 3 :',subtitle:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus ad praesentium nam! Totam sapiente vitae porro dolores, maiores, autem pariatur fuga doloribus.',link:'#',githubLink:'#',moreLink:'#'},
-  {index:4,cat:'js',imgPath:'./1.jpg',title:'Project Name 4 :',subtitle:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus ad praesentium nam! Totam sapiente vitae porro dolores, maiores, autem pariatur fuga doloribus.',link:'#',githubLink:'#',moreLink:'#'},
-  {index:5,cat:'js',imgPath:'./1.jpg',title:'Project Name 5 :',subtitle:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus ad praesentium nam! Totam sapiente vitae porro dolores, maiores, autem pariatur fuga doloribus.',link:'#',githubLink:'#',moreLink:'#'},
-
+  {index:1,cat:'html&css',imgPath:'./1.png',title:'Leon',subtitle:'A modern, minimalistic website template for "Leon" agency, showcasing services, projects, and contact details with a clean design.',link:'https://reboussama.github.io/Leon/',githubLink:'https://github.com/RebOussama/Leon',moreLink:'#'},
+  {index:2,cat:'html&css',imgPath:'./2.png',title:'Sushiman',subtitle:'A clean, modern website template for "Sushiman," featuring Japanese food and drinks with sections for menu, services, and contact details.',link:'https://reboussama.github.io/Sushiman/',githubLink:'https://github.com/RebOussama/Sushiman',moreLink:'#'},
+  {index:3,cat:'react',imgPath:'./3.png',title:'.REACT',subtitle:'A sleek, modern landing page for ".REACT," offering data analytics solutions with different pricing plans and a simple, professional design.',link:'https://reboussama.github.io/.REACT/',githubLink:'https://github.com/RebOussama/.REACT',moreLink:'#'},
+  {index:4,cat:'react',imgPath:'./4.png',title:'Portfolio',subtitle:'A minimalistic portfolio website showcasing creative projects with a clean design and sections for services, portfolio, and contact information.',link:'https://reboussama.github.io/Portfolio/',githubLink:'https://github.com/RebOussama/Portfolio',moreLink:'#'},
+  {index:5,cat:'react',imgPath:'./5.png',title:'Rebooks',subtitle:'Rebooks" is an online marketplace for buying, selling, and exchanging books. It offers a simple platform for book lovers to find new and second-hand books',link:'https://reboussama.github.io/Rebooks/',githubLink:'https://github.com/RebOussama/Rebooks',moreLink:'#'},
+  {index:6,cat:'js',imgPath:'./6.png',title:'Javascript Projects',subtitle:'Calculator,Hangman Game,Memory Game,Quiz App and Typing Test',link:'https://reboussama.github.io/Javascript-Projects/',githubLink:'https://github.com/RebOussama/Javascript-Projects',moreLink:'#'},
+  
 
 ]
 
@@ -56,8 +57,8 @@ const Main = () => {
             <p className="subtitle">{item.subtitle}</p>
             <div className="icons flex">
               <div style={{gap:"15px"}} className="flex linko">
-                <div className="icon icon-link"></div>
-                <div className="icon icon-github"></div>
+                <a href={item.link} target="_blank" className="icon icon-link"></a>
+                <a href={item.githubLink} target="_blank" className="icon icon-github"></a>
               </div>
               <a className="flex" href={item.moreLink}>
                 more 
